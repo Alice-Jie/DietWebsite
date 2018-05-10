98b7eeee8fa37d72191f284ed160c24569854b5e
Alice_Jie <909011298@qq.com>
2018-05-10 14:57:47 +0800

    添加SQL文件

---
+++ SQL/CREATE_TABLE_food.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `NewTable` (
+  `id`  int(11) NOT NULL AUTO_INCREMENT ,
+  `name`  varchar(20) NULL ,
+  `price`  double(5,2) NULL ,
+  `description`  varchar(255) NULL ,
+  PRIMARY KEY (`id`)
+);
